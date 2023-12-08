24c0b220bd1fc51c0dfa689a856153a3d0050a3e
nibirietz <nibirietz@gmail.com>
2023-12-08 18:57:30 +0300

    Rewrite to SQLAlchemy.

--- db.py
+++ db.py
@@ -1,44 +1,44 @@
-import sqlite3
+import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, select
+from sqlalchemy.orm import declarative_base, Session
+
+Base = declarative_base()
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    status = Column(Integer)
+    created_on = Column(DateTime)
+    start_date = Column(DateTime)
+
+    def __init__(self, name, status=-1, start_date=None):
+        self.name = name
+        self.status = status
+        self.created_on = datetime.datetime.now()
+        self.start_date = start_date
 
 
 class Database:
     def __init__(self):
-        self.db_name = "task.db"
+        self.engine = create_engine("sqlite:///task.db", echo=True)
+        Base.metadata.create_all(self.engine)
+        self.session = Session(self.engine)
 
-    def add_task(self, task):
-        connection = sqlite3.connect(self.db_name)
-        cursor = connection.cursor()
-        cursor.execute(f"""INSERT INTO tasks(name, status, start_date)
-        VALUES (?, ?, ?);""", (task.name, task.status, task.start_date))
-        connection.commit()
-        cursor.close()
-        connection.close()
+    def add_task(self, task: Task):
+        self.session.add(task)
+        self.session.commit()
 
-    def update_task(self, task):
-        connection = sqlite3.connect(self.db_name)
-        cursor = connection.cursor()
-        cursor.execute(f"""UPDATE tasks
-        SET name = ?, status = ?, start_date = ?;""", (task.name, task.status, task.start_date))
-        connection.commit()
-        cursor.close()
-        connection.close()
+    def load_tasks(self) -> list[type(Task)]:
+        return [task for task in self.session.query(Task)]
 
-    def load_tasks(self):
-        connection = sqlite3.connect(self.db_name)
-        cursor = connection.cursor()
-        cursor.execute(f"""SELECT * FROM tasks;""")
-        result = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return result
-
-    def delete_task(self, task):
-        connection = sqlite3.connect(self.db_name)
-        cursor = connection.cursor()
-        cursor.execute(f"""DELETE FROM tasks WHERE name = "{task.name}";""")
-        connection.commit()
-        cursor.close()
-        connection.close()
+    def delete_task(self, task: Task):
+        self.session.delete(task)
+        self.session.commit()
 
 
-db = Database()
+database = Database()
+print(database.load_tasks())
--- main.py
+++ main.py
@@ -1,55 +1,41 @@
 import flet
-from db import *
-
+from db import Task
+import db
 
 class MainWindow(flet.UserControl):
     def __init__(self):
         super().__init__()
         self.page = None
-        self.repository = Repository(db)
         flet.app(target=self.main_loop)
 
     def main_loop(self, page: flet.Page):
         self.page = page
         self.add_input_field()
-        self.load_date()
+        self.load_data()
 
     def add_input_field(self):
         input_field = flet.TextField(label="Введите задачу", on_submit=self.create_task)
         self.page.add(input_field)
 
-    def load_date(self):
-        tasks = self.repository.load_tasks()
+    def load_data(self):
+        tasks = db.database.load_tasks()
         for task in tasks:
-            print()
             self.page.add(TaskView(task).get_view())
 
     def create_task(self, field):
         task_name = field.control.value
-        task = Task(task_name)
+        task = Task(name=task_name)
         task_view = TaskView(task)
-        self.repository.add_task(task)
+        db.database.add_task(task)
         self.page.add(task_view.get_view())
 
 
-class Task:
-    def __init__(self, name: str, status=0, start_date=None):
-        self.name = name
-        self.status = status
-        self.start_date = start_date
-
-    def delete_task(self):
-        repo = Repository(db)
-        repo.delete_task(self)
-
-
-class TaskView(flet.UserControl):
+class TaskView:
     def __init__(self, task: Task):
-        super().__init__()
         self.task = task
         name = flet.Text(value=task.name)
         status = flet.Text(value=str(task.status))
-        start_date = flet.Text(value=task.start_date)
+        start_date = flet.Text(value=str(task.start_date))
         delete_button = flet.ElevatedButton(text="D", on_click=self.delete_task)
         self.row = flet.Row([name, status, start_date, delete_button])
 
@@ -57,34 +43,8 @@ class TaskView(flet.UserControl):
         return self.row
 
     def delete_task(self, task):
-        self.task.delete_task()
+        db.database.delete_task(self.task)
         self.row.page.remove(self.row)
-
-
-class Repository:
-    def __init__(self, database: Database):
-        self.database = database
-
-    def add_task(self, task: Task):
-        self.database.add_task(task)
-
-    def update_task(self, task: Task):
-        self.database.update_task(task)
-
-    def load_tasks(self):
-        tasks = []
-        data = self.database.load_tasks()
-        for task_row in data:
-            name = task_row[1]
-            status = task_row[2]
-            start_date = task_row[3]
-            task = Task(name, status, start_date)
-            tasks.append(task)
-
-        return tasks
-
-    def delete_task(self, task):
-        self.database.delete_task(task)
 
 
 def main():
